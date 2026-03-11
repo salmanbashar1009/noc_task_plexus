dde7d0858e14211dab596807c802ada04c4cd516
salmanbashar1009 <salmanbashar101@gmail.com>
2026-03-11 20:02:03 +0600

    usecase file added

---
+++ lib/core/usecases/usecase.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import '../error/failures.dart';
+
+// ignore: avoid_types_as_parameter_names
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams {
+  const NoParams();
+}
