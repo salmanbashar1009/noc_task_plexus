54eb3e5d7ba169fe1d781e0970a84ec16042210e
salmanbashar1009 <salmanbashar101@gmail.com>
2026-03-14 14:31:29 +0600

    feat: device events are added

---
+++ lib/features/devices/presentation/bloc/device_event.dart
@@ -0,0 +1,17 @@
+import 'package:equatable/equatable.dart';
+
+abstract class DeviceEvent extends Equatable {
+  const DeviceEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class StartMonitoring extends DeviceEvent {}
+
+class FilterDevices extends DeviceEvent {
+  final String query;
+  final bool? onlineStatus; // true for online, false for offline, null for all
+
+  const FilterDevices(this.query, {this.onlineStatus});
+}
